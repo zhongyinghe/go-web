42941f47541457a217f6574ddfcb08d260164d29
zhongyinghe <642188160@qq.com>
2017-10-30 17:45:07 +0800

    核心解析xml代码示例

---
+++ xml/fileXml.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"log"
+)
+
+type Result struct {
+	Person []Person
+}
+
+type Person struct {
+	Name      string
+	Age       int
+	Career    string
+	Interests Interests
+}
+
+type Interests struct {
+	Interest []string
+}
+
+func main() {
+	content, err := ioutil.ReadFile("studygolang.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result Result
+	err = xml.Unmarshal(content, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(result)
+
+	for _, p := range result.Person {
+		log.Println(p.Name, p.Age, p.Career, p.Interests.Interest)
+		/*for _, i := range p.Interests.Interest {
+			log.Println(i)
+		}*/
+	}
+}
