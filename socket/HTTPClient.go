5ff7184cfa7374f121f09b3fff64906a6275760b
zhongyinghe <642188160@qq.com>
2017-11-08 11:28:05 +0800

    TCP入门代码

---
+++ socket/HTTPClient.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	//"bytes"
+	"fmt"
+	//"io"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
+		os.Exit(1)
+	}
+
+	service := os.Args[1]
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+
+	//result, err := readFully(conn)
+	result, err := ioutil.ReadAll(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+/*func readFully(conn net.Conn) ([]byte, error) {
+	defer conn.Close()
+
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("over...")
+				break
+			}
+			return nil, err
+		}
+	}
+	return result.Bytes(), nil
+}*/
